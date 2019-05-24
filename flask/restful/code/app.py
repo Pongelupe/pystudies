a5ebeee39fbce4d2c39a4f24382a396fc6306275
Pongelupe <pongelupe1998@gmail.com>
2019-05-23 22:05:33 -0300

    first things using JWT for authenticate

--- flask/restful/code/app.py
+++ flask/restful/code/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
+app.secret_key = 'felagund'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
